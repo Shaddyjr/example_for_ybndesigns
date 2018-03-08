04a26d2ebf82438065c1302f6da3f249343930d6
mahdiASC <mahdi@allstarcode.org>
2018-03-07 20:43:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/example/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.android.example;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
+    Animation animation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.strike);
+        animation = AnimationUtils.loadAnimation(this, R.anim.blink);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            public void onAnimationStart(Animation animation) {}
+            public void onAnimationRepeat(Animation animation) {}
+            public void onAnimationEnd(Animation animation) {
+                img.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    public void flash(View v){
+        img.setVisibility(View.VISIBLE);
+        img.startAnimation(animation);
+    }
+}
